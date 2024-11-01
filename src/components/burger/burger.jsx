30faa80d15de864ba65f29bd5f8af126369d57e5
Lana <lolhowko@gmail.com>
2024-11-01 19:19:08 +0300

    add first page

--- src/components/burger/burger.jsx
+++ src/components/burger/burger.jsx
@@ -7,28 +7,35 @@ export function Burger() {
 
   return (
     <div>
-      <div className="nav__burger burger" onClick={toggleVisibility}>
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
-        <span className="burger__line"></span>
-      </div>
+      {visible && (
+        <div className="nav__burger burger" onClick={toggleVisibility}>
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
+        </div>
+      )}
 
       {!visible && (
         <div className="nav__menu menu">
           <ul className="menu__list">
             <li className="menu__item">
               <a href="#" className="menu__link">
-                Главное
+                ГЛАВНАЯ
               </a>
             </li>
             <li className="menu__item">
               <a href="#" className="menu__link">
-                О компании
+                ПОРТФОЛИО
               </a>
             </li>
             <li className="menu__item">
               <a href="../signin.html" className="menu__link">
-                Портфолио
+                ПУБЛИКАЦИИ
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="../signin.html" className="menu__link">
+                КОНТАКТЫ
               </a>
             </li>
           </ul>
