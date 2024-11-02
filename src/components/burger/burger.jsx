7ff417c2bf6d3b9366b83112566f65096cb1df23
Lana <lolhowko@gmail.com>
2024-11-02 07:19:21 +0300

    add full burger

--- src/components/burger/burger.jsx
+++ src/components/burger/burger.jsx
@@ -23,11 +23,48 @@ export function Burger() {
                 ГЛАВНАЯ
               </a>
             </li>
-            <li className="menu__item">
-              <a href="#" className="menu__link">
-                ПОРТФОЛИО
-              </a>
-            </li>
+
+            <div className="menu__item_big">
+              <li className="menu__item">
+                <a href="#" className="menu__link">
+                  ПОРТФОЛИО
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="#" className="menu__link_small">
+                  ИНТЕРЬЕРЫ
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="#" className="menu__link_small">
+                  ВЫСТАВКИ
+                </a>
+              </li>
+            </div>
+
+            <div className="menu__item_big">
+              <li className="menu__item">
+                <a href="../signin.html" className="menu__link">
+                  О СТУДИИ
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="../signin.html" className="menu__link_small">
+                  КОМАНДА
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="../signin.html" className="menu__link_small">
+                  ОТЗЫВЫ
+                </a>
+              </li>
+              <li className="menu__item">
+                <a href="../signin.html" className="menu__link_small">
+                  РЕКВИЗИТЫ
+                </a>
+              </li>
+            </div>
+
             <li className="menu__item">
               <a href="../signin.html" className="menu__link">
                 ПУБЛИКАЦИИ
@@ -39,6 +76,26 @@ export function Burger() {
               </a>
             </li>
           </ul>
+
+          <div className="menu__contact">
+            <a
+              href="mailto:info@deluxdesign.ru"
+              className="menu__contact__link"
+            >
+              info@deluxdesign.ru
+            </a>
+            <div className="">
+              <p className="menu__contact__link">Александр</p>
+              <p className="menu__contact__link">8 900 245 16 30</p>
+            </div>
+
+            <div className="">
+              <p className="menu__contact__link">Светлана</p>
+              <p className="menu__contact__link">8 906 037 96 56</p>
+            </div>
+
+            <button className="menu__contact__button">НАПИСАТЬ НАМ</button>
+          </div>
         </div>
       )}
     </div>
